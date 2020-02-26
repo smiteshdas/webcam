6e516b2d4c4a1cf8c9ca1ae2ab25974f39fd961d
Smitesh Das <57407736+smiteshdas@users.noreply.github.com>
2020-02-26 10:06:42 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,30 @@
+let video;
+let classifier;
+let div;
+
+
+
+function setup(){
+  noCanvas()
+  div = createP('Loading..')
+  
+  video= createCapture(VIDEO);
+  classifier = ml5.imageClassifier('MobileNet',video, modelLoaded);
+  
+  }
+
+function modelLoaded(){
+  div.html('Model is Ready')
+  classifier.classify(gotResults);
+  }
+
+function gotResults(err,results){
+  if(err){
+    div.html('Error')
+    }else{
+  
+      div.html(results[0].label)
+      classifier.classify(gotResults)
+      }
+  
+    }
